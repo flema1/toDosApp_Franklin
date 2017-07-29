4da298853f7606c88e188ad99cd2fe8565d1ac39
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-07-29 16:48:06 -0400

    views, routes initial set up

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const path = require('path');
 const app = express()
 
 app.use(logger('dev'));
+// static files
+app.use(express.static('public'));
+
+// views
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 
-app.get('/', function (req, res) {
-  res.send('Hello Franklin!')
-})
+app.get('/', (req, res) => {
+  res.render('index', {
+    message: 'Hello world we are aalive!',
+  });
+});
 
 app.listen(3000, function () {
   console.log('app listening on port 3000!')
@@ -17,6 +25,10 @@ app.listen(3000, function () {
 app.get('/toDos', (req, res) => {
   res.send('Info about toDos!');
 });
+
+//additional routes
+ const todoRoutes = require('./routes/todo-routes');
+ app.use('/toDos/', todoRoutes);
 
 
 
---
+++ routes/todo-routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const todoRoutes = express.Router();
+
+todoRoutes.get('/saturday', (req, res) => {
+  res.send('Yea, its Saturday!');
+});
+ 
+module.exports = todoRoutes;
