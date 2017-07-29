2bad131b6cd3c1452a4a59296bdcc41a26378d36
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-07-29 17:21:35 -0400

    inital display of all todos, app is alive

--- routes/todo-routes.js
+++ routes/todo-routes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const todoRoutes = express.Router();
 
+const todosController = require('../controllers/todos-controller');
+
+
 todoRoutes.get('/saturday', (req, res) => {
   res.send('Yea, its Saturday!');
 });
+
+
+todoRoutes.get('/all', todosController.index);
+
  
 module.exports = todoRoutes;
