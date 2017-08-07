c132d5a3ae85aca31cc2df7c1229b06d0ba8fe5f
Franklin Lema <Franklin.Lema93@myhunter.cuny.edu>
2017-08-06 23:38:07 -0400

    Auth is sucessful

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.get('/', (req, res) => {
 
 
 /* setting up port & listen */
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, function() {
   console.log(`listening on port ${PORT} 00`);
 });
